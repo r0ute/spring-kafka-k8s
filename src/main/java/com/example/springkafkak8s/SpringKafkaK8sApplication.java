427020c4148cc70dee5aaff01b83fdf03de9f70b
r0ute <route-a@ya.ru>
2024-02-07 21:25:25 -0600

    init project

---
+++ src/main/java/com/example/springkafkak8s/SpringKafkaK8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkak8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaK8sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springkafkak8s/SpringKafkaK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkak8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
